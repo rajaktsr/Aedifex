b7fcb0f175ea1af38203542a8719c150202eeee0
ohinisar <rajakautsar119@gmail.com>
2025-01-17 15:37:08 +0700

    pembuatan website statis selesai

--- Aedifex1/Controllers/HomeController.cs
+++ Aedifex1/Controllers/HomeController.cs
@@ -26,21 +26,40 @@ namespace Aedifex1.Controllers
         public IActionResult Products()
         {
             var products = new List<Product>
-            {
-                new Product { Image = "/assets/catalog1.jpg", Name = "Product 1" },
-                new Product { Image = "/assets/catalog2.jpg", Name = "Product 2" },
-                new Product { Image = "/assets/catalog3.jpg", Name = "Product 3" },
-                new Product { Image = "/assets/catalog4.jpg", Name = "Product 4" },
-                new Product { Image = "/assets/catalog5.jpg", Name = "Product 5" },
-                new Product { Image = "/assets/catalog6.jpg", Name = "Product 6" },
-                new Product { Image = "/assets/catalog7.jpg", Name = "Product 7" },
+    {
+        new Product {Id = 1,  Image = "/assets/catalog1.jpg", Name = "", Category = "Varsity Jacket" },
+        new Product {Id = 2,  Image = "/assets/catalog2.jpg", Name = "", Category = "Varsity Jacket" },
+        new Product {Id = 3,  Image = "/assets/catalog3.jpg", Name = "", Category = "Varsity Jacket" },
+        new Product {Id = 4,  Image = "/assets/catalog4.jpg", Name = "", Category = "Varsity Jacket" },
+        new Product {Id = 5,  Image = "/assets/catalog5.jpg", Name = "", Category = "Varsity Jacket" },
+        new Product {Id = 6,  Image = "/assets/catalog6.jpg", Name = "", Category = "Varsity Jacket" },
+        new Product {Id = 7,  Image = "/assets/catalog7.jpg", Name = "", Category = "Varsity Jacket" },
+        new Product {Id = 8,  Image = "/assets/catalog8.jpg", Name = "", Category = "Work Jacket" },
+        new Product {Id = 9,  Image = "/assets/catalog9.jpg", Name = "", Category = "Work Jacket" },
+        new Product {Id = 10, Image = "/assets/catalog10.jpg", Name = "", Category = "Work Jacket" },
+        new Product {Id = 11, Image = "/assets/catalog11.jpg", Name = "", Category = "Work Jacket" },
+        new Product {Id = 12, Image = "/assets/catalog12.jpg", Name = "", Category = "Work Jacket" },
+        new Product {Id = 13, Image = "/assets/catalog13.jpg", Name = "", Category = "Work Jacket" },
+        new Product {Id = 14, Image = "/assets/catalog14.jpg", Name = "", Category = "Work Shirt" },
+        new Product {Id = 15, Image = "/assets/catalog15.jpg", Name = "", Category = "Hoodie & Crewneck" },
+        new Product {Id = 16, Image = "/assets/catalog16.jpg", Name = "", Category = "Hoodie & Crewneck" },
+        new Product {Id = 17, Image = "/assets/catalog17.jpg", Name = "", Category = "Hoodie & Crewneck" },
+        new Product {Id = 18, Image = "/assets/catalog18.jpg", Name = "", Category = "Hoodie & Crewneck" },
+        new Product {Id = 19, Image = "/assets/catalog19.jpg", Name = "", Category = "Coach Jacket" },
+        new Product {Id = 20, Image = "/assets/catalog20.jpg", Name = "", Category = "Coach Jacket" },
+        new Product {Id = 21, Image = "/assets/catalog21.jpg", Name = "", Category = "Coach Jacket" },
+        new Product {Id = 22, Image = "/assets/catalog22.jpg", Name = "", Category = "Coach Jacket" },
+        new Product {Id = 23, Image = "/assets/catalog23.jpg", Name = "", Category = "Coach Jacket" },
+        new Product {Id = 24, Image = "/assets/catalog24.jpg", Name = "", Category = "T-Shirt" },
+        new Product {Id = 25, Image = "/assets/catalog25.jpg", Name = "", Category = "T-Shirt" },
 
-             
-                // Tambahkan lebih banyak produk jika perlu
-            };
+
+        // Tambahkan lebih banyak produk jika diperlukan
+    };
 
             return View(products);
         }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
--- Aedifex1/Models/Product.cs
+++ Aedifex1/Models/Product.cs
@@ -2,6 +2,7 @@
 {
     public class Product
     {
+        public int Id { get; set; }
         public string Image { get; set; }
         public string Name { get; set; }
         public string Category { get; set; } // Tambahkan kategori
